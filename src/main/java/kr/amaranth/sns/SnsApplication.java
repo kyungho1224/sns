8e715b13e38fc4b2b40a8af885c5e91f7f9a976b
kimkyungho <dev_kimkyungho@kakao.com>
2024-02-26 17:30:55 +0900

    init : 초기 DB 설정 제외 부분 주석 추가

--- src/main/java/kr/amaranth/sns/SnsApplication.java
+++ src/main/java/kr/amaranth/sns/SnsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)	// 초기 DB 설정을 제외하고 애플리케이션을 실행
 public class SnsApplication {
 
 	public static void main(String[] args) {
