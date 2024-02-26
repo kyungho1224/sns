a502f113bb9f7919d076c5e44bc1a5a43893cc6c
kimkyungho <dev_kimkyungho@kakao.com>
2024-02-26 17:26:46 +0900

    init : initialize project

---
+++ src/test/java/kr/amaranth/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.amaranth.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
